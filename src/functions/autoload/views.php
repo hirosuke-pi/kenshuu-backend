351a5c7078fda21f7e93687909b719423f841e9d
hirosuke-pi <betacode.net@gmail.com>
2023-05-24 15:10:10 +0900

    fix コンフリクト解消

--- src/functions/autoload/views.php
+++ src/functions/autoload/views.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/../const.php';
 require_once __DIR__ . '/../common.php';
 
 require_once __DIR__ . '/../component.php';
-require_once __DIR__ . '/viewComponent.php';
+require_once __DIR__ . '/../viewComponent.php';
---
+++ src/functions/const.php
@@ -0,0 +1,10 @@
+<?php
+
+const CSRF_NAME = 'csrf_token';
+const METHOD_NAME = '_method';
+
+
+const DB_NAME = 'kenshuu_backend';
+const DB_USER = 'postgres';
+const DB_PASSWORD = 'secret';
+const DB_HOST = 'database';
