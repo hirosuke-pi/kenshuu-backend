039c5a6db1861a17f7991d3ebdd9dfebec6368f1
hirosuke-pi <betacode.net@gmail.com>
2023-05-30 12:36:56 +0900

    fix エラーページ調整

--- src/components/pages/error.php
+++ src/components/pages/error.php
@@ -21,7 +21,8 @@ $headProps = ['title' => 'Flash News'];
 <?=$head->view($headProps)?>
     <body>
         <?=$header->view()?>
-        <main class="flex justify-center flex-col items-center">
+        <main class="flex justify-center flex-col items-center mx-3">
+            <img class="h-60 my-5" src="https://3.bp.blogspot.com/-f2csyxp_K2o/VuKEBEj7NjI/AAAAAAAA4w4/jVcA_kX6sbcXu3O5R5muNVOlN1DdyW2kA/s800/pet_angel_cat.png" />
             <h2 class="text-2xl">エラー: <?=$error->values->message ?></h2>
             <a href="/" class="mt-5 bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded ">
                 <i class="fa-solid fa-arrow-left"></i> ホームへ戻る
