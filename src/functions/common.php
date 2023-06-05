dc73f13065d1d7e2fa213823099e0be94642b122
hirosuke-pi <betacode.net@gmail.com>
2023-06-05 11:22:53 +0900

    add コメント追加

--- src/functions/common.php
+++ src/functions/common.php
@@ -1,11 +1,24 @@
 <?php
 
-function var_log($dump) {
+/**
+ * var_dumpを見やすい形式に出力
+ *
+ * @param mixed $dump 出力したいオブジェクト
+ * @return void
+ */
+function var_log(mixed $dump) {
     echo '<pre>';
     var_dump($dump);
     echo '</pre>';
 }
 
+/**
+ * キーとその型をチェック
+ *
+ * @param array $params チェックしたい配列
+ * @param array $requirePropKeys チェックするキーとその型
+ * @return void
+ */
 function checkKeyTypes(array $params, array $requirePropKeys) {
     foreach ($requirePropKeys as $key => $type) {
         if (!array_key_exists($key, $params)) {
@@ -19,6 +32,12 @@ function checkKeyTypes(array $params, array $requirePropKeys) {
     }
 }
 
+/**
+ * DateTime形式の文字列を'2023/04/23 18:32:12'の形式に変換する
+ *
+ * @param string $datetime DateTime形式の文字列
+ * @return string 変換済みの文字列
+ */
 function getDateTimeFormat(string $datetime): string {
     $now = new DateTime($datetime);
     return $now->format('Y/m/d H:i:s');
